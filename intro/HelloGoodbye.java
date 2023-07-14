3afdf76933858d81b28d1241b2c4729b2781f1a1
jsattari <jsattari3@gmail.com>
2023-07-14 09:27:51 -0700

    added new folder for intro assignments

--- intro/HelloGoodbye.java
+++ intro/HelloGoodbye.java
@@ -1,4 +1,11 @@
 public class HelloGoodbye {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Names are not provided as arguments");
+    } else {
+      System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+      System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+  }
 }
